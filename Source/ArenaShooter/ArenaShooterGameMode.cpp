e784cc260e3e58b559a39b78e3c1b70701626e68
Victor Kataev <victor.kataev@victoriavr.com>
2023-01-03 18:25:15 +0100

    initial commit, brand new project with a blank plugin

---
+++ Source/ArenaShooter/ArenaShooterGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ArenaShooterGameMode.h"
+#include "ArenaShooterCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AArenaShooterGameMode::AArenaShooterGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
